abfee21c9364ddcd00be7db93314997055f4315b
gmeghnag <gmeghnag@redhat.com>
2021-10-05 17:45:44 +0200

    stable

--- app/app.py
+++ app/app.py
@@ -1,16 +1,24 @@
 import os
 import json
-from flask import Flask, render_template, make_response, request
-app = Flask(__name__)
 
+
+from flask import Flask, render_template, make_response, request, jsonify
+import requests
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/', methods=['GET'])
-def index():
-    
-    _headers = dict(request.headers)
+def get_index():
+    headers = dict(request.headers)
     resp = make_response(render_template("index.html", headers=headers))
     resp.headers['Content-Type'] = 'text/html'
     return (resp, 200)
 
+@app.route('/', methods=['POST', 'OPTIONS'])
+def post_index():
+    headers = dict(request.headers)
+    return jsonify(headers)
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
