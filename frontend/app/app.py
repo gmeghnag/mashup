ee37b8b298a8aef7c56b70a3cd49b93f43b5d674
gmeghnag <gmeghnag@redhat.com>
2021-10-06 18:30:27 +0200

    v1.1

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -5,7 +5,8 @@ import requests
 from flask import Flask, render_template, make_response, request, jsonify
 
 app = Flask(__name__)
-app.config['TEMPLATES_AUTO_RELOAD'] = True
+#app.config['TEMPLATES_AUTO_RELOAD'] = True
+#app.config['DEBUG'] = True
 
 FOODS_MICROSERVICE_HOST = os.environ.get('FOODS_MICROSERVICE_HOST')
 FOODS_MICROSERVICE_PORT = os.environ.get('FOODS_MICROSERVICE_PORT')
@@ -26,11 +27,11 @@ def post_foods():
     if FOODS_MICROSERVICE_HOST is not None and FOODS_MICROSERVICE_PORT is not None:
         _headers = dict(request.headers)
         for key, value in _headers.items():
-            if key.startswith("X-"):
+            if key.startswith("Z-"):
                 headers[key] = value
         r = requests.get(f'{FOODS_MICROSERVICE_HOST}:{FOODS_MICROSERVICE_PORT}/', headers=headers)
         data = r.json()
     return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
