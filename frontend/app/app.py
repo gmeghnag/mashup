ecf6fd5de4afa8f3ac6a5aafb4f75d6ab142cd46
gmeghnag <gmeghnag@redhat.com>
2021-10-07 12:38:45 +0200

    v1.2

--- frontend/app/app.py
+++ frontend/app/app.py
@@ -1,37 +1,41 @@
 import os
 import json
+from pprint import pprint as pr
 
 import requests
-from flask import Flask, render_template, make_response, request, jsonify
+from flask import Flask, render_template, Response, request, jsonify
 
 app = Flask(__name__)
-#app.config['TEMPLATES_AUTO_RELOAD'] = True
-#app.config['DEBUG'] = True
+
 
 FOODS_MICROSERVICE_HOST = os.environ.get('FOODS_MICROSERVICE_HOST')
 FOODS_MICROSERVICE_PORT = os.environ.get('FOODS_MICROSERVICE_PORT')
+
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template("index.html")
 
-@app.route('/headers', methods=['POST', 'OPTIONS'])
-def post_headers():
-    headers = dict(request.headers)
-    return jsonify(headers)
 
 @app.route('/foods', methods=['POST', 'OPTIONS'])
 def post_foods():
+    data = {"items": []}
     headers = {}
-    data = {"items": ["pizza", "pasta", "arancini", "tapas", "croquetas", "paella"]}
+    response_headers = {}
+    _headers = dict(request.headers)
+    for key, value in _headers.items():
+        if key.startswith("X-Mashup-"):
+            headers[key] = value
+            response_headers[key] = value
+            continue
+        if key.startswith("X-"):
+            response_headers[key] = value
     if FOODS_MICROSERVICE_HOST is not None and FOODS_MICROSERVICE_PORT is not None:
-        _headers = dict(request.headers)
-        for key, value in _headers.items():
-            if key.startswith("Z-"):
-                headers[key] = value
         r = requests.get(f'{FOODS_MICROSERVICE_HOST}:{FOODS_MICROSERVICE_PORT}/', headers=headers)
         data = r.json()
-    return jsonify(data)
+    resp = Response(json.dumps(data))
+    resp.headers = response_headers
+    return resp
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
